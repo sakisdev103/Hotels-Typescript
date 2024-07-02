a84591dec5275c1ba801284254614bf4ea76c23e
Your Name <sakistr69@gmail.com>
2024-07-02 16:08:19 +0300

    Updated the ui of calendar

--- src/components/Dates.tsx
+++ src/components/Dates.tsx
@@ -11,16 +11,12 @@ import { DateRange } from "react-day-picker";
 
 //Ui
 import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
 import { Calendar } from "@/components/ui/calendar";
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
-
-//Icon
-import { Calendar as CalendarIcon } from "lucide-react";
 
 export function Dates({ className }: React.HTMLAttributes<HTMLDivElement>) {
   const initialRange = useSelector((state: RootState) => state.dates);
@@ -41,34 +37,13 @@ export function Dates({ className }: React.HTMLAttributes<HTMLDivElement>) {
     }
   }, [date]);
 
+  const [calendarOpen, setCalendarOpen] = React.useState(true);
+
   return (
     <div className={cn("grid gap-2", className)}>
-      <Popover>
-        <PopoverTrigger asChild>
-          <Button
-            id="date"
-            variant={"outline"}
-            className={cn(
-              "w-[300px] justify-start text-left font-normal",
-              !date && "text-muted-foreground"
-            )}
-          >
-            <CalendarIcon className="mr-2 h-4 w-4" />
-            {date?.from ? (
-              date.to ? (
-                <>
-                  {format(date.from, "LLL dd, y")} -{" "}
-                  {format(date.to, "LLL dd, y")}
-                </>
-              ) : (
-                format(date.from, "LLL dd, y")
-              )
-            ) : (
-              <span>Pick a date</span>
-            )}
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent className="w-auto p-0" align="start">
+      <Popover open={calendarOpen} onOpenChange={setCalendarOpen}>
+        <PopoverTrigger />
+        <PopoverContent className="w-auto " sideOffset={-15}>
           <Calendar
             initialFocus
             mode="range"
